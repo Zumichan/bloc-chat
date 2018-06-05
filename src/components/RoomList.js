59d7296b5c04af125523102c91d09336cd462b69
Azumi Callanan <azumi@Azumis-MacBook-Air.local>
2018-06-05 09:26:49 -0700

    Checkpoint List Chat Rooms:In progress

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 
 class RoomList extends Component {
   constructor (props) {
-    super(props)
+    super(props);
     this.state = {
       rooms: []
     }
     this.roomsRef = this.props.firebase.database().ref('rooms');
+
+    
   }
+
+
+
 }
 
 export default RoomList;
