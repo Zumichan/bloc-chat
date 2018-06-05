681552c0b0a78992793a6db57454f63061fa17ab
Azumi Callanan <azumi@Azumis-MacBook-Air.local>
2018-06-05 09:23:46 -0700

    Checkpoint List Chat Rooms:In progress

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+var config = {
+  apiKey: "AIzaSyA8iZaf7j67xSX4rApcRe2YBiQadut4XCk",
+  authDomain: "bloc-chat-b40f7.firebaseapp.com",
+  databaseURL: "https://bloc-chat-b40f7.firebaseio.com",
+  projectId: "bloc-chat-b40f7",
+  storageBucket: "bloc-chat-b40f7.appspot.com",
+  messagingSenderId: "1029301371739"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
---
+++ src/components/RoomList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RoomList extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      rooms: []
+    }
+    this.roomsRef = this.props.firebase.database().ref('rooms');
+  }
+}
+
+export default RoomList;
